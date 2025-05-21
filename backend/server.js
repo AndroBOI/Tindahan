542f8feeee432705cc6fa0f8747ef7521429610a
AndroBOI <andrewjovellanos48@gmail.com>
2025-05-21 21:14:20 +0800

    working database and endpoints

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import productRoutes from '../backend/routes/productRoutes.js'
+import {sql} from '../backend/config/db.js'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use('/api/products', productRoutes)
+
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(225) NOT NULL,
+                image VARCHAR(225) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `
+
+        console.log('Database initialize successfully')
+    } catch (err) {
+        console.error('error initDB', err)
+    }
+}
+
+initDB().then(()=> {
+    app.listen(PORT, ()=> {
+    console.log(`server is running on port ${PORT}`)
+})
+})
