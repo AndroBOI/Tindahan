ccc5ea7de20ec8e4744425cb77ea767dd45122b8
AndroBOI <andrewjovellanos48@gmail.com>
2025-05-22 14:11:28 +0800

    second commit adding the frontend and completed the backend shits like security and stuff

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import productRoutes from '../backend/routes/productRoutes.js'
 import {sql} from '../backend/config/db.js'
+import {aj} from '../backend/lib/arcjet.js'
+
 
 dotenv.config()
 const app = express()
@@ -15,6 +17,36 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan('dev'))
 app.use(cors())
+
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, {
+            requested: 1
+        }) 
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) {
+                res.status(429).json({
+                    error: "Too many Requests"
+                })
+            } else if(decision.reason.isBot()) {
+                res.status(403).json({error: "Bot access denied "})
+            } else {
+                res.status(403).json({error: "Forbidden"})
+            }
+            return
+        }
+
+        if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+            res.status(403).json({error: "Spoofed bot detected"})
+        }
+
+        next()
+
+    } catch (e) {
+        console.log("Arcjet", e) 
+        next(e)
+    }
+})
 
 app.use('/api/products', productRoutes)
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      
+    </div>
+  )
+}
+
+export default App
