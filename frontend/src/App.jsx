13c7089b8f2e445391ddaa572b6533922ccab03e
AndroBOI <andrewjovellanos48@gmail.com>
2025-05-25 12:21:27 +0800

    dropdown theme selector done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import Navbar from '../components/Navbar'
-function App() {
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+import { useThemeStore } from "./store/useThemeStore"
+export default function App() {
 
+  const {theme} = useThemeStore()
 
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
-      
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar/>
+      <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/products' element={<ProductPage/>}/>
+      </Routes>
     </div>
   )
 }
-
-export default App
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProductPage() {
   return (
     <div>
-      
+      <p>Product</p>
     </div>
   )
 }
